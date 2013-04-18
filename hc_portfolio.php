cf0184da78bcbcefdf00e8ccc089a33dccf3c864
Don Denton <don@happycollision.com>
2013-04-18 16:44:59 -0400

    Initial Commit. Apparently.

---
+++ hc_portfolio.php
@@ -0,0 +1,393 @@
+<?php
+/*
+Plugin Name: Happy Collision Portfolio
+Description: Model for a media portfolio.
+Author: Don Denton
+Version: 1.0
+Author URI: http://happycollision.com
+Depends: Simple Fields
+*/
+
+
+
+if(!function_exists('simple_fields_register_field_group')):
+
+if(function_exists('hcwarn')){
+	hcwarn('Happy Collision Portfolio cannot run because Simple Fields is not installed. Or Simple Fields is installed, but not loading early enough. Try clicking the "Plugins" link on the left-hand side of the screen. That will reload this page the proper way. If this message disappears, you are fine. If not, contact your developer.',true);
+}
+
+function hc_portfolio(){
+	return false;
+}
+
+
+else:
+
+function hc_portfolio(){
+	return true;
+}
+
+
+Class HCPortfolio{
+	// All the 'name' fields should be unique. Or at least, no two 'name' fields of different 'type' values should be the same. This could cause data to be mistaken for something else and overwritten.
+	public static $all_simple_fields =
+		array(
+			'video'=>array(
+				array(
+					'slug' => "hc_video_name",
+					'name' => 'Video Name',
+					'description' => 'The name of the video clip you are uploading. For example: "Oklahoma Slideshow" or "Oklahoma Clips".',
+					'type' => 'text'
+				)
+				,array(
+					'slug' => "hc_video_image",
+					'name' => 'Placeholder Image',
+					'description' => 'Upload an image file as a placeholder for the video.',
+					'type' => 'file'
+				)
+				,array(
+					'slug' => "hc_video_mp4",
+					'name' => 'MP4 Video',
+					'description' => 'Upload the .mp4 version here, if you have it.',
+					'type' => 'file'
+				)
+				,array(
+					'slug' => "hc_video_webm",
+					'name' => 'WEBM Video',
+					'description' => 'Upload the .webm version here, if you have it.',
+					'type' => 'file'
+				)
+				,array(
+					'slug' => "hc_video_ogg",
+					'name' => 'OGG/Theora Video',
+					'description' => 'Upload the .ogg version here, if you have it.',
+					'type' => 'file'
+				)
+				,array(
+					'slug' => "hc_video_flv",
+					'name' => 'FLV Video',
+					'description' => 'Upload the .flv (or .f4v) version here, if you have it.',
+					'type' => 'file'
+				)
+				,array(
+					'slug' => "hc_video_mov",
+					'name' => 'MOV or M4V Video',
+					'description' => 'Upload the .mov or .m4v version here, if you have it.',
+					'type' => 'file'
+				)
+			)
+			,'audio'=>array(
+				array(
+					'slug' => "hc_audio_name",
+					'name' => 'Audio Name',
+					'description' => 'The name of the audio clip you are uploading. For example: "H2$ Overture".',
+					'type' => 'text'
+				)
+				,array(
+					'slug' => "hc_audio",
+					'name' => 'MP3 Audio',
+					//'description' => '',
+					'type' => 'file'
+				)
+			)
+			,'image_gallery'=>array(
+				array(
+					'slug' => "hc_image_gallery_item",
+					'name' => 'Gallery Image',
+					//'description' => '',
+					'type' => 'file'
+				)
+			)
+			,'single_image'=>array(
+				array(
+					'slug' => "hc_image_item",
+					'name' => 'Single Image',
+					//'description' => '',
+					'type' => 'file'
+				)
+			)
+		);
+	
+	//Just easiest to manually copy from above. Seriously.
+	private static $html5_field_names = 
+		array(
+			'MP4 Video'
+			,'WEBM Video'
+			,'OGG/Theora Video'
+		);
+	private static $video_types = 
+		array(
+			'mp4'=>array(
+				'html5'=>true
+				,'mime'=>'video/mp4'
+			)
+			,'webm'=>array(
+				'html5'=>true
+				,'mime'=>'video/webm'
+			)
+			,'ogv'=>array(
+				'html5'=>true
+				,'mime'=>'video/ogg'
+			)
+			,'flv'=>array(
+				'html5'=>false
+				,'mime'=>'video/flv'
+			)
+			,'f4v'=>array(
+				'html5'=>false
+				,'mime'=>'video/flv'
+			)
+			,'mov'=>array(
+				'html5'=>false
+				,'mime'=>'video/quicktime'
+			)
+			,'m4v'=>array(
+				'html5'=>false
+				,'mime'=>'video/quicktime'
+			)
+		);
+	private static $file_field_names;
+	
+	private $video_data;
+	private $have_html5 = false;
+	private $audio_data;
+	private $project_data;
+	private $display_array;
+	
+	//Will contain arrays of ready to print HTML.
+	private $all_items_output = array();
+	private $video_items_output = array();
+	private $audio_items_output = array();
+		
+	
+	function __construct($post_id){
+		if(self::$file_field_names == null) self::file_field_names();
+		$this->video_data = $this->get_video_values($post_id);
+		$this->audio_data = $this->get_audio_values($post_id);
+		$this->project_data = simple_fields_get_post_group_values($post_id, "Project Properties", true, 2);
+		
+		$this->display_array = array(
+			'videos' => $this->video_data
+			,'audio' => $this->audio_data
+		);
+	}
+	
+	private function get_video_values($post_id){
+		$videos = simple_fields_get_post_group_values($post_id, "Video Item(s)", true, 2);
+		$this->check_for_html5_video($videos);
+		$this->hc_portfolio_prepare_file_paths($videos);
+		return $videos;
+	}
+
+	private function get_audio_values($post_id){
+		$audio = simple_fields_get_post_group_values($post_id, "Audio Item(s)", true, 2);
+		$this->hc_portfolio_prepare_file_paths($audio);
+		return $audio;
+	}
+
+	private function un_ms_file_rewrite_path($url_to_file){
+		//Fix iOS issue if this blog uses ms-file rewrite. If not, this code does no harm.
+		
+		$basedir = wp_upload_dir();
+		$basedir = $basedir['basedir'];
+		$date_folder = NULL;
+		preg_match('#/[0-9]{4}/[0-1]{1}[0-9]{1}#', $url_to_file, $date_folder);
+		$path_to_file = $basedir . $date_folder[0] . '/' . basename($url_to_file);
+		//at this point, we have the full unix path to the file. This is bad on the internet.
+		
+		//strip the unnecessary unix stuff at the beginning and replace it with the domain name
+		$path_to_file = strstr($path_to_file, 'wp-content');
+		
+		//build the domain-based path
+		$url_build = wp_upload_dir();
+		$url_build = $url_build['baseurl'];
+		$url_build = substr($url_build, 0, strpos($url_build, 'wp-content'));
+		
+		return $url_build . $path_to_file;
+	}
+
+	private function hc_portfolio_prepare_file_paths(&$array_of_file_data){
+		//changing variable name for clarity
+		$files = &$array_of_file_data;
+
+		foreach($files as &$file){
+			
+			foreach($file as $field_name => &$field){
+				//Is this an attachment field? Wouldn't want to accidentally just look up a random URL for what is supposed to actually be a number.
+				if(!in_array($field_name, self::$file_field_names)) continue;
+				if($field == 0) {
+					//remove unused URL fields
+					unset($file[$field_name]);
+					continue;
+				}
+				
+				$field = $this->un_ms_file_rewrite_path(wp_get_attachment_url($field));
+				
+			}
+		}			
+	}
+	
+	private static function file_field_names(){
+		$output = array();
+		$field_group_array = self::$all_simple_fields;
+		foreach($field_group_array as $field_group){
+			foreach($field_group as $field){
+				if($field['type'] === 'file'){
+					$output[] = $field['name'];
+				}
+			}
+		}
+		self::$file_field_names = $output;
+	}
+	
+	private function check_for_html5_video($videos){
+		foreach($videos as $video_data)
+		//Check if we have an HTML5 video available
+		foreach(static::$html5_field_names as $type){
+			if($video_data[$type] != false){
+				//we have at least one HTML5 video type
+				$this->have_html5 = true;
+				break;
+			}
+		}
+	}
+	
+	public function echo_video($title_tag = null, $width = null, $height = null){
+		if(empty($this->video_items_output)) $this->prepare_video_output($title_tag,$width,$height);
+		
+		foreach($this->video_items_output as $output) echo $output;
+	}
+	
+	private function prepare_video_output($title_tag=null,$width=null,$height=null){
+		if(empty($this->video_data)) return;
+		foreach($this->video_data as $clip){
+			unset($sources);
+			$player_video_url = '';
+			$output_buffer = '';
+			foreach($clip as $field_name => $property){
+				if(!in_array($field_name, self::$file_field_names)) continue;
+				$ext = pathinfo($property,PATHINFO_EXTENSION);
+				
+				if(isset(self::$video_types[$ext])){
+					if(self::$video_types[$ext]['html5']==true){
+						$sources[] = '<source src="'.$property.'" type="'.self::$video_types[$ext]['mime'].'" />';
+					}else{
+						$player_video_url = $property;
+						break;
+					}
+				}
+			}
+			//Move on if there is no video to see here.
+			if(!isset($sources) && $player_video_url === '') continue;
+			
+			$title = $this->prepare_item_title('video',$title_tag,$clip);
+			if(isset($clip['Placeholder Image'])){
+				$init = "<video width=\"{$width}\" height=\"{$height}\" controls poster=\"{$clip['Placeholder Image']}\">";
+			}else{
+				$init = "<video width=\"{$width}\" height=\"{$height}\" controls>";
+			}
+		
+			
+			$player = 
+				'<object width="100%" height="360" type="application/x-shockwave-flash" data="'.plugins_url('/flowplayer-3.2.15.swf', __FILE__).'">'.
+					'<param name="movie" value="'.plugins_url('/flowplayer-3.2.15.swf', __FILE__).'" />'.
+					'<param name="allowfullscreen" value="true" />'.
+					'<param name="flashvars" value="config={\'clip\': {\'url\': \''.$player_video_url.'\', \'autoPlay\':false, \'autoBuffering\':true, \'scaling\':\'fit\'}, \'canvas\':{\'backgroundColor\':\'#000\'}}" />'.
+				'</object>';
+			
+			$destroy = '</video>';
+			
+			//Okay. Plug it all in.
+			$output_buffer = $title;
+			if(isset($sources)){
+				$output_buffer .= $init;
+				foreach($sources as $source){
+					$output_buffer .= $source;
+				}
+			}
+			if($player_video_url !== ''){
+				$output_buffer .= $player;
+			}
+			if(isset($sources)){
+				$output_buffer .= $destroy;
+			}
+			
+			$this->video_items_output[] = $output_buffer;
+			$this->all_items_output[] = $output_buffer;
+		}
+	}
+	
+	private function prepare_audio_output($title_tag=null,$width=null,$height=null){
+		if(empty($this->audio_data)) return;
+		foreach($this->audio_data as $clip){
+			$title = $this->prepare_item_title('audio',$title_tag,$clip);
+			$output_buffer = 
+				"$title
+				<audio controls>
+					<source src=\"{$clip['MP3 Audio']}\" type=\"\" />
+				</audio>";
+			$this->audio_items_output[] = $output_buffer;
+			$this->all_items_output[] = $output_buffer;
+		}
+	}
+
+	private function prepare_item_title($kind,$title_tag='h4',$single_array){
+		if($title_tag==null) $title_tag = 'h4';
+		return "<{$title_tag}>" . $this->get_item_title($kind,$single_array) . "</{$title_tag}>";
+	}
+	
+	private function get_item_title($kind, $single_array){
+		switch ($kind):
+			case 'video':
+				return $single_array['Video Name'];
+				break;
+			case 'audio':
+				return $single_array['Audio Name'];
+				break;
+		endswitch;
+	}
+	
+	private function populate_all_items(){
+		$this->prepare_video_output();
+		$this->prepare_audio_output();
+	}
+	
+	//Public Functions
+	public function title($id=null){
+		//Title of projet is stored as Wordpress Post Title
+		echo get_the_title($id=null);
+	}
+	
+	public function description(){
+		echo $this->project_data['Project Description'];
+	}
+	
+	public function location(){
+		echo $this->project_data['Project Location'];
+	}
+	
+	public function date($format=null){
+		if($format == null){
+			echo $this->project_data['Project Date'];
+			return;
+		}
+		echo date($format,strtotime($this->project_data['Project Date']));
+	}
+	
+	public function all_items($tag=null,$class=null){
+		if(empty($this->all_items_output)) $this->populate_all_items();
+		$tag = $tag == null ? 'div' : $tag;
+		$class = $class == null ? 'item' : $class;
+		foreach($this->all_items_output as $item){
+			echo "<{$tag} class=\"{$class}\">$item</{$tag}>";
+		}
+	}
+	
+	public function count_items(){
+		if(empty($this->all_items_output)) $this->populate_all_items();
+		return count($this->all_items_output);
+	}
+}
+require_once('simple_fields_call.php');
+
+endif;
---
+++ simple_fields_call.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Adds a new field group
+ *
+ * Usage:
+ * simple_fields_register_field_group( string unique_field_group_slug, array field_groups );
+ * 
+ * Each field group looks like this:
+ * 
+ * 	array (
+ * 		'name' => 'Name of field group',
+ * 		'description' => "A short description of the field group",
+ * 		'repeatable' => TRUE or FALSE if the field group should be repeatable,
+ * 		'fields' => array of fields,
+ * 		'deleted' => TRUE or FALSE
+ *	),
+ * 	array ( another field group array ),
+ * 	array ( another field group array ),
+ * 	array ( ...)
+ *
+ * Each field array looks like this:
+ * 	array(
+ * 		'slug' => "unique_field_slug"
+ * 		'name' => 'Field name',
+ *		'description' => 'Field description',
+ *		'type' => 'field type',
+ * 		'deleted' => TRUE or FALSE
+ *	)
+ * 
+ * @param string $slug the slug of this field group. must be unique.
+ * @param array $new_field_group settings/options for the new group
+ * @return array the new field group as an array
+ */
+simple_fields_register_field_group('project_properties',
+	array (
+		'name' => 'Project Properties',
+		'description' => "The name of the overarching project should be entered at the top of the page. The items you will add below are from that project. For example, if it is a video of scenes from Oklahoma, the project name would be Oklahoma.",
+		'repeatable' => 0,
+		'fields' => array(
+			array(
+				'slug' => "hc_project_location",
+				'name' => 'Project Location',
+				'description' => 'Examples: "Illinois Wesleyan" "Florida State University" "Post Playhouse"',
+				'type' => 'text'
+			)
+			,array(
+				'slug' => "hc_project_date",
+				'name' => 'Project Date',
+				'description' => 'Optional',
+				'type' => 'date_v2',
+				"options" => array(
+					"date_v2" => array(
+						"show" => "on_click",
+						"show_as" => "date",
+						"default_date" => "no_date"
+					)
+				)
+			)			
+			,array(
+				'slug' => "hc_project_description",
+				'name' => 'Project Description',
+				'description' => 'Optional',
+				'type' => 'textarea',
+				'type_textarea_options' => array('use_html_editor' => 0)
+			)
+		)
+	)
+);
+
+simple_fields_register_field_group('portfolio_item_video',
+	array (
+		'name' => 'Video Item(s)',
+		'description' => 'Add one or more videos to your project portfolio. You can (and probably should) upload the same exact video in multiple formats. If you have different videos (one that is a slideshow of images and one that is actual footage of the show, for example), click the "+ Add" button again to upload the second clip.',
+		'repeatable' => 1,
+		'fields' => HCPortfolio::$all_simple_fields['video']
+	)
+);
+
+simple_fields_register_field_group('portfolio_item_audio',
+	array (
+		'name' => 'Audio Item(s)',
+		'description' => "Add one or more audio clips to your project portfolio. The only acceptable audio type is .mp3, so be sure it is converted to that.",
+		'repeatable' => 1,
+		'fields' => HCPortfolio::$all_simple_fields['audio']
+	)
+);
+
+simple_fields_register_field_group('portfolio_item_image_gallery',
+	array (
+		'name' => 'Image Gallery',
+		'description' => "Add a bunch of images to a single gallery.",
+		'repeatable' => 1,
+		'fields' => HCPortfolio::$all_simple_fields['image_gallery']
+	)
+);
+
+simple_fields_register_field_group('portfolio_item_image',
+	array (
+		'name' => 'Single Image',
+		'description' => "One single image. Perhaps your project was a painting? This field will be treated slightly different from the Featured Image.",
+		'repeatable' => 0,
+		'fields' => HCPortfolio::$all_simple_fields['single_image']
+	)
+);
+
+/*
+			,array(
+				'slug' => "my_text_field_slug",
+				'name' => 'Test text',
+				'description' => 'Text description',
+				'type' => 'text'
+			)
+			,array(
+				'slug' => "my_textarea_field_slug",
+				'name' => 'Test textarea',
+				'description' => 'Textarea description',
+				'type' => 'textarea',
+				'type_textarea_options' => array('use_html_editor' => 1)
+			)
+			,array(
+				'slug' => "my_checkbox_field_slug",
+				'name' => 'Test checkbox',
+				'description' => 'Checkbox description',
+				'type' => 'checkbox',
+				'type_checkbox_options' => array('checked_by_default' => 1)
+			)
+			,array(
+				'slug' => "my_radiobutton_field_slug",
+				'name' => 'Test radiobutton',
+				'description' => 'Radiobutton description',
+				'type' => 'radiobutton',
+				'type_radiobutton_options' => array(
+					array("value" => "Yes"),
+					array("value" => "No")
+				)
+			)
+			,array(
+				'slug' => "my_dropdown_field_slug",
+				'name' => 'Test dropdown',
+				'description' => 'Dropdown description',
+				'type' => 'dropdown',
+				'type_dropdown_options' => array(
+					"enable_multiple" => 1,
+					"enable_extended_return_values" => 1,
+					array("value" => "Yes"),
+					array("value" => "No")
+				)
+			)
+			,array(
+				'slug' => "my_file_field_slug",
+				'name' => 'Test file',
+				'description' => 'File description',
+				'type' => 'file'
+			)
+			,array(
+				'slug' => "my_post_field_slug",
+				'name' => 'Test post',
+				'description' => 'Post description',
+				'type' => 'post',
+				'type_post_options' => array("enabled_post_types" => array("post"))
+			)
+			,array(
+				'slug' => "my_taxonomy_field_slug",
+				'name' => 'Test taxonomy',
+				'description' => 'Taxonomy description',
+				'type' => 'taxonomy',
+				'type_taxonomy_options' => array("enabled_taxonomies" => array("category"))
+			)
+			,array(
+				'slug' => "my_taxonomyterm_field_slug",
+				'name' => 'Test taxonomy term',
+				'description' => 'Taxonomy term description',
+				'type' => 'taxonomyterm',
+				'type_taxonomyterm_options' => array("enabled_taxonomy" => "category")
+			)
+			,array(
+				'slug' => "my_color_field_slug",
+				'name' => 'Test color selector',
+				'description' => 'Color selector description',
+				'type' => 'color'
+			)
+			,array(
+				'slug' => "my_date_field_slug",
+				'name' => 'Test date selector',
+				'description' => 'Date selector description',
+				'type' => 'date',
+				'type_date_options' => array('use_time' => 1)
+			)
+			,array(
+				'slug' => "my_date2_field_slug",
+				'name' => 'Test date selector',
+				'description' => 'Date v2 selector description',
+				'type' => 'date_v2',
+				"options" => array(
+					"date_v2" => array(
+						"show" => "on_click",
+						"show_as" => "datetime",
+						"default_date" => "today"
+					)
+				)
+			)			
+			,array(
+				'slug' => "my_user_field_slug",
+				'name' => 'Test user selector',
+				'description' => 'User selector description',
+				'type' => 'user'
+			)
+*/
+
+
+// function simple_fields_register_post_connector($unique_name = "", $new_post_connector = array()) {
+simple_fields_register_post_connector('portfolio_connector',
+	array (
+		'name' => "Portfolio Connector",
+		'field_groups' => array(
+			array(
+				'slug' => 'project_properties',
+				'context' => 'normal',
+				'priority' => 'high'
+			)
+			,array(
+				'slug' => 'portfolio_item_video',
+				'context' => 'normal',
+				'priority' => 'high'
+			)
+			,array(
+				'slug' => 'portfolio_item_audio',
+				'context' => 'normal',
+				'priority' => 'high'
+			)
+/*			//The model for these items is not complete/necessary yet.
+			,array(
+				'slug' => 'portfolio_item_image_gallery',
+				'context' => 'normal',
+				'priority' => 'high'
+			)
+			,array(
+				'slug' => 'portfolio_item_image',
+				'context' => 'normal',
+				'priority' => 'high'
+			)
+*/
+		),
+		'post_types' => array('post'),
+		'hide_editor' => true
+	)
+);
+
+/**
+ * Sets the default post connector for a post type
+ * 
+ * @param $post_type_connector = connector id (int) or slug (string) or string __inherit__
+ * 
+ */
+simple_fields_register_post_type_default('portfolio_connector', 'post');
